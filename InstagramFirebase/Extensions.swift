02bd674ce984ff3c9680788be27ba892c6668abf
Chris Paterson <CJPaterson.3@gmail.com>
2017-06-30 20:06:55 +0100

    Added extension for UIView.addConstraintsWithFormat

--- InstagramFirebase/Extensions.swift
+++ InstagramFirebase/Extensions.swift
@@ -13,3 +13,21 @@ extension UIColor {
         return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: alpha)
     }
 }
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: format,
+            options: NSLayoutFormatOptions(),
+            metrics: nil,
+            views: viewsDictionary))
+    }
+}
